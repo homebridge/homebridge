396b56e3a535ad1f946f4898811eb8431cd539bf
Jon Maddox <jon@jonmaddox.com>
2015-06-25 02:00:37 -0400

    add methods to Wink accessory to fetch brightness and power state

--- platforms/Wink.js
+++ platforms/Wink.js
@@ -56,6 +56,40 @@ function WinkAccessory(log, device) {
 }
 
 WinkAccessory.prototype = {
+  getPowerState: function(callback){
+    if (!this.device) {
+      this.log("No '"+this.name+"' device found (yet?)");
+      return;
+    }
+
+    var that = this;
+
+    this.log("checking power state for: " + this.name);
+    wink.user().device(this.name, function(light_obj){
+      powerState = light_obj.desired_state.powered
+      that.log("power state for " + that.name + " is: " + powerState)
+      callback(powerState);
+    });
+
+
+  },
+
+  getBrightness: function(callback){
+    if (!this.device) {
+      this.log("No '"+this.name+"' device found (yet?)");
+      return;
+    }
+
+    var that = this;
+
+    this.log("checking brightness level for: " + this.name);
+    wink.user().device(this.name, function(light_obj){
+      level = light_obj.desired_state.brightness * 100
+      that.log("brightness level for " + that.name + " is: " + level)
+      callback(level);
+    });
+
+  },
 
   setPowerState: function(powerOn) {
     if (!this.device) {
