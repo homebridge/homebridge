5611e38bebd6f6efb8973f05d12053e19dfa1d7f
Nick Farina <nfarina@gmail.com>
2015-07-15 22:36:45 -0700

    Provider config and cli-based setup

--- example-providers/homebridge-lockitron/index.js
+++ example-providers/homebridge-lockitron/index.js
@@ -1,3 +1,24 @@
+import request from 'request';
 
 // Demonstrate that we were loaded
 console.log("Lockitron provider loaded!");
+
+module.exports = {
+  
+  config: {
+    accessToken: {
+      type: 'string',
+      description: "You can find your personal Access Token at: https://api.lockitron.com",
+      required: true
+    }
+  },
+  
+  validateConfig: function(callback) {
+    
+    // validate the accessToken
+    let accessToken = homebridge.config.get('homebridge-lockitron.accessToken');
+    
+    // prove that we got a value
+    console.log(`Access Token: ${accessToken}`);
+  }
+}
