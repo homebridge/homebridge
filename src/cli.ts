76716a6e0ed77ffcbf7da15d7fd80e1c136bb0f5
Andi <mail@anderl-bauer.de>
2020-04-14 06:03:51 +0200

    Add node version check for homebridge required node version (#2495)

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,20 @@
 import "source-map-support/register"; // registering node-source-map-support for typescript stack traces
 import commander from "commander";
-import getVersion from "./version";
+import getVersion, { getRequiredNodeVersion } from "./version";
 import { Logger } from "./logger";
 import { User } from "./user";
 import { HomebridgeOptions, Server } from "./server";
 import { init } from "hap-nodejs";
 import Signals = NodeJS.Signals;
+import { satisfies } from "semver";
 
 const log = Logger.internal;
+
+const requiredNodeVersion = getRequiredNodeVersion();
+if (requiredNodeVersion && !satisfies(process.version, requiredNodeVersion)) {
+  log.warn(`Homebridge requires Node version of ${requiredNodeVersion} which does \
+not satisfy the current Node version of ${process.version}. You may need to upgrade your installation of Node.`);
+}
 
 // noinspection JSUnusedGlobalSymbols
 export = function cli(): void {
