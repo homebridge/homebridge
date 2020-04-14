87880c4ffa28513cb7248cb9e4ceb284d888c48e
Supereg <mail@anderl-bauer.de>
2020-04-14 03:29:00 +0200

    Add node version check for homebridge required node version

--- src/version.spec.ts
+++ src/version.spec.ts
@@ -1,4 +1,4 @@
-import getVersion from "./version";
+import getVersion, { getRequiredNodeVersion } from "./version";
 import fs, { PathLike } from "fs";
 import path from "path";
 
@@ -25,7 +25,35 @@ describe("version", () => {
       });
 
       const version = getVersion();
-      expect(version).toBe(version);
+      expect(version).toBe(expectedVersion);
+    });
+  });
+
+  describe(getRequiredNodeVersion, () => {
+    it("should read correct node version from package.json", function() {
+      const expectedVersion = ">=10.17.0";
+      const expectedPath = path.resolve(__dirname, "../package.json");
+
+      const mock = jest.spyOn(fs, "readFileSync");
+      // mock only once, otherwise we break the whole test runner
+      mock.mockImplementationOnce((path: PathLike | number, options?: { encoding?: string | null; flag?: string } | string | null) => {
+        expect(path).toBe(expectedPath);
+        expect(options).toBeDefined();
+        expect(typeof options).toBe("object");
+        const opt = options as {encoding: string};
+        expect(opt.encoding).toBe("utf8");
+
+        const fakeJson = {
+          engines: {
+            node: expectedVersion,
+          },
+        };
+
+        return JSON.stringify(fakeJson, null, 4); // pretty print
+      });
+
+      const version = getRequiredNodeVersion();
+      expect(version).toBe(expectedVersion);
     });
   });
 });
--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,17 @@
 import fs from "fs";
 import path from "path";
 
-export default function getVersion(): string {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function loadPackageJson(): any {
   const packageJSONPath = path.join(__dirname, "../package.json");
   const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath, { encoding: "utf8" }));
-  return packageJSON.version;
+  return packageJSON;
+}
+
+export default function getVersion(): string {
+  return loadPackageJson().version;
+}
+
+export function getRequiredNodeVersion(): string {
+  return loadPackageJson().engines.node;
 }
