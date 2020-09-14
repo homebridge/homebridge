58093cf1314356b6fbd05306841d8ff6dbb3f2ce
Supereg <mail@anderl-bauer.de>
2020-09-14 21:57:22 +0200

    Prepare v1.2.0 release

--- .github/workflows/prerelease.js
+++ .github/workflows/prerelease.js
@@ -1,26 +1,35 @@
 #!/bin/env node
 
-const fs = require('fs');
-const semver = require('semver');
-const child_process = require('child_process');
+const fs = require("fs");
+const semver = require("semver");
+const child_process = require("child_process");
+
+const packageJSON = JSON.parse(fs.readFileSync("package.json", "utf8"));
 
 function getTagVersionFromNpm(tag) {
   try {
-    return child_process.execSync(`npm info ${package.name} version --tag="${tag}"`).toString('utf8').trim();
+    return child_process.execSync(`npm info ${packageJSON.name} version --tag="${tag}"`).toString("utf8").trim();
   } catch (e) {
-    return null;
+    throw e;
   }
 }
 
-// load package.json
-const package = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+// betaVersion is a custom property we put into the package.json to indicate which release we want to tag
+const projectBetaVersion = packageJSON.betaVersion;
+if (!projectBetaVersion) {
+  throw new Error("Unable to calculate the next prerelease version. 'betaVersion' was not set in the package.json")
+}
 
-// work out the correct tag
-const currentLatest = getTagVersionFromNpm('latest') || '0.0.0';
-const currentBeta = getTagVersionFromNpm('beta') || '0.0.0';
-const latestNpmTag = semver.gt(currentBeta, currentLatest, { includePrerelease: true }) ? currentBeta : currentLatest;
-const publishTag = semver.gt(package.version, latestNpmTag, { includePrerelease: true }) ? package.version : latestNpmTag;
+const latestReleaseBeta = getTagVersionFromNpm("beta"); // like 0.7.0-beta.12
+const betaAsRelease = semver.inc(latestReleaseBeta, "patch"); // will produce 0.7.0 (needed for the equality check below)
+
+let publishTag;
+if (semver.eq(projectBetaVersion, betaAsRelease)) { // check if we are releasing another version for the latest beta
+  publishTag = latestReleaseBeta; // set the current latest beta to be incremented
+} else {
+  publishTag = projectBetaVersion; // start of with a new beta version
+}
 
 // save the package.json
-package.version = publishTag;
-fs.writeFileSync('package.json', JSON.stringify(package, null, 2));
+packageJSON.version = publishTag;
+fs.writeFileSync("package.json", JSON.stringify(packageJSON, null, 2));
