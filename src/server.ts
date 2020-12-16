b9d81459fe02a8229424967c42cbcf1be7af31f5
Supereg <mail@anderl-bauer.de>
2020-12-16 23:22:07 +0100

    Add mdns.legacyAdvertiser config option

--- src/server.ts
+++ src/server.ts
@@ -195,6 +195,7 @@ export class Server {
       bind: bridgeConfig.bind,
       mdns: this.config.mdns, // this is deprecated now
       addIdentifyingMaterial: true,
+      useLegacyAdvertiser: this.config.mdns?.legacyAdvertiser,
     };
 
     if (bridgeConfig.setupID && bridgeConfig.setupID.length === 4) {
@@ -630,6 +631,7 @@ export class Server {
         bind: this.config.bridge.bind,
         mdns: this.config.mdns, // this is deprecated and not used anymore
         addIdentifyingMaterial: true,
+        useLegacyAdvertiser: this.config.mdns?.legacyAdvertiser,
       }, this.allowInsecureAccess);
     });
   }
