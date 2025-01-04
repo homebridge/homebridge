49296f8042f2dbc6ff80e790ba85fb3e36e68d37
Vvorcun <tvferb@gmail.com>
2025-01-04 14:45:44 +0600

    Make it possible to set Characteristic.SerialNumber via config.json

--- src/bridgeService.ts
+++ src/bridgeService.ts
@@ -188,7 +188,7 @@ export class BridgeService {
     const info = this.bridge.getService(Service.AccessoryInformation)!;
     info.setCharacteristic(Characteristic.Manufacturer, bridgeConfig.manufacturer || "homebridge.io");
     info.setCharacteristic(Characteristic.Model, bridgeConfig.model || "homebridge");
-    info.setCharacteristic(Characteristic.SerialNumber, bridgeConfig.username);
+    info.setCharacteristic(Characteristic.SerialNumber, bridgeConfig.serialNumber || bridgeConfig.username);
     info.setCharacteristic(Characteristic.FirmwareRevision, bridgeConfig.firmwareRevision || getVersion());
 
     this.bridge.on(AccessoryEventTypes.LISTENING, (port: number) => {
