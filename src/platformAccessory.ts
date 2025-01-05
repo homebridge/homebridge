ea9e8955c38e11edbee207e87d7d213745592130
Just Jam <renzo.zanelli6@gmail.com>
2025-01-05 22:35:10 +0000

    Added method updateDisplayName()

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -90,6 +90,13 @@ export class PlatformAccessory<T extends UnknownContext = UnknownContext>  exten
     });
   }
 
+  public updateDisplayName(name: string): void {
+    if (name) {
+      this.displayName = name;
+      this._associatedHAPAccessory.displayName = name;
+    }
+  }
+
   public addService(service: Service): Service;
   public addService<S extends typeof Service>(serviceConstructor: S, ...constructorArgs: ConstructorArgs<S>): Service;
   public addService(service: Service | typeof Service, ...constructorArgs: any[]): Service { // eslint-disable-line @typescript-eslint/no-explicit-any
