6e229be6bbaacd873bb99e9e357600afaf6cc1ee
Ben <43026681+bwp91@users.noreply.github.com>
2025-06-08 01:04:22 +0100

    general code modernisation (#3681)

--- src/user.ts
+++ src/user.ts
@@ -1,38 +1,36 @@
-import os from "os";
-import path from "path";
+import { homedir } from 'node:os'
+import { join, resolve } from 'node:path'
 
 /**
  * Manages user settings and storage locations.
  */
 export class User {
-
-  private static customStoragePath?: string;
-  private static storageAccessed = false;
+  private static customStoragePath?: string
+  private static storageAccessed = false
 
   static configPath(): string {
-    return path.join(User.storagePath(), "config.json");
+    return join(User.storagePath(), 'config.json')
   }
 
   static persistPath(): string {
-    return path.join(User.storagePath(), "persist"); // hap-nodejs data is stored here
+    return join(User.storagePath(), 'persist') // hap-nodejs data is stored here
   }
 
   static cachedAccessoryPath(): string {
-    return path.join(User.storagePath(), "accessories");
+    return join(User.storagePath(), 'accessories')
   }
 
   static storagePath(): string {
-    User.storageAccessed = true;
+    User.storageAccessed = true
 
-    return User.customStoragePath ? User.customStoragePath : path.join(os.homedir(), ".homebridge");
+    return User.customStoragePath ? User.customStoragePath : join(homedir(), '.homebridge')
   }
 
   public static setStoragePath(...storagePathSegments: string[]): void {
     if (User.storageAccessed) {
-      throw new Error("Storage path was already accessed and cannot be changed anymore. Try initializing your custom storage path earlier!");
+      throw new Error('Storage path was already accessed and cannot be changed anymore. Try initializing your custom storage path earlier!')
     }
 
-    User.customStoragePath = path.resolve(...storagePathSegments);
+    User.customStoragePath = resolve(...storagePathSegments)
   }
-
 }
