0b8bbb8daccea8851de2550a9193e91689f4dfca
Tal <talrhv1@gmail.com>
2026-03-29 15:10:34 +0300

    fixed matter cache getter

--- src/matter/server/ServerLifecycle.ts
+++ src/matter/server/ServerLifecycle.ts
@@ -45,7 +45,7 @@ export interface ServerLifecycleDeps {
   commissioningManager: CommissioningManager
   fabricManager: FabricManager
   getCommissioningDeps: () => CommissioningDeps
-  accessoryCache: MatterAccessoryCache | null
+  getAccessoryCache: () => MatterAccessoryCache | null
   setAccessoryCache: (cache: MatterAccessoryCache) => void
   setServerNode: (node: ServerNode | null) => void
   getServerNode: () => ServerNode | null
@@ -357,8 +357,9 @@ export class ServerLifecycle {
 
     await this.waitForServerReady(deps)
 
-    if (deps.accessoryCache) {
-      const loaded = await deps.accessoryCache.load()
+    const cache = deps.getAccessoryCache()
+    if (cache) {
+      const loaded = await cache.load()
       log.debug(`Matter cache loaded: ${loaded.size} accessories`)
     } else {
       log.debug('No accessory cache available')
@@ -400,8 +401,9 @@ export class ServerLifecycle {
 
     try {
       // Save accessory cache before shutting down
-      if (deps.accessoryCache && accessories.size > 0) {
-        await deps.accessoryCache.save(accessories)
+      const cache = deps.getAccessoryCache()
+      if (cache && accessories.size > 0) {
+        await cache.save(accessories)
         log.debug('Saved accessory cache before shutdown')
       }
 
